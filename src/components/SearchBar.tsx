9776e5cfbb4fab780c21047b511f54f90d018b89
soy eddy <soyeddy.email.com>
2025-01-27 20:36:51 -0600

    estilos componente search

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -13,15 +13,20 @@ const SearchBar: React.FC = () => {
   };
 
   return (
-    <div className="flex items-center space-x-2 ">
+    <div className="flex items-center gap-2 ">
       <input
         type="text"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
         placeholder="Buscar en la Biblia..."
-        className="border rounded-lg p-2 w-full"
+        className="border rounded-lg p-2 "
       />
-      <Button onClick={handleSearch} className="p-2  rounded-lg transition duration-300">Buscar</Button>
+      <Button
+        onClick={handleSearch}
+        className="p-2  rounded-lg transition duration-300"
+      >
+        Buscar
+      </Button>
     </div>
   );
 };
