3182e262cd10f22e2cd73fbe5e9a488724bd59f0
soy eddy <soyeddy.email.com>
2025-01-29 22:10:21 -0600

    retirando padding inncesario

--- src/components/home/TabHome.tsx
+++ src/components/home/TabHome.tsx
@@ -40,7 +40,7 @@ export const TabHome = () => {
   };
 
   return (
-    <div className="w-full max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
+    <div className="w-full max-w-4xl mx-auto sm:px-6 lg:px-8">
       <Tabs
         value={activeTab}
         onValueChange={handleTabChange}
