50144618bc59e334ac585245892147bfb863fedb
soy eddy <soyeddy.email.com>
2025-02-06 21:17:54 -0600

    configurando dotenv y github-workflow

---
+++ cypress/e2e/Login.cy.ts
@@ -0,0 +1,22 @@
+describe("Login Form", () => {
+    beforeEach(() => {
+      cy.visit("http://localhost:5173/login"); 
+    });
+  
+    it("Debería cargar la página de login", () => {
+      cy.contains("Inicia sesión para continuar").should("be.visible");
+    });
+  
+    it("Debería permitir escribir email y contraseña", () => {
+      cy.get('input[type="email"]').type("usuario@example.com");
+      cy.get('input[type="password"]').type("123456");
+    });
+  
+    it("El botón de login debería estar habilitado después de escribir", () => {
+      cy.get('input[type="email"]').type("usuario@example.com");
+      cy.get('input[type="password"]').type("123456");
+      cy.get("button[type='submit']").should("not.be.disabled");
+    });
+  
+  });
+  
