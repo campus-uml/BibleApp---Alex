0c7c0d910d7cc3d25358530894441ea20fb6669f
soy eddy <soyeddy.email.com>
2025-02-10 22:44:21 -0600

    configurando cypress agregando carpetas support

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vitest/config"
+import path from "path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [react()],
@@ -11,6 +11,10 @@ export default defineConfig({
   },
   test: {
     globals: true,
-    environment: 'jsdom',
+    environment: "jsdom",
+    setupFiles: ["./setupTest.ts"],
+    coverage: {
+      provider: "v8",
+    },
   },
-})
+});
