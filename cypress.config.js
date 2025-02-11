dcb9d8822183e62fe16030c00ae2c6747fe106bf
soy eddy <soyeddy.email.com>
2025-02-10 22:34:27 -0600

    correcion id del proyecto

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "huddwf",
+  projectId: "2k6vty",
   e2e: {
     baseUrl: "http://localhost:5173",
   },
