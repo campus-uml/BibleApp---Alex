8c318a26cb566f951ddbe76281039928df29e5c5
soy eddy <soyeddy.email.com>
2025-02-11 21:04:03 -0600

    condiguraciones en cypress y vite config para acceder a las variables de entorno supabase

--- cypress.config.js
+++ cypress.config.js
@@ -4,5 +4,21 @@ export default defineConfig({
   projectId: "2k6vty",
   e2e: {
     baseUrl: "http://localhost:5173",
+    setupNodeEvents(on, config) {
+      config.env.VITE_API_SUPABASE_URL = process.env.VITE_API_SUPABASE_URL;
+      config.env.VITE_API_SUPABASE_ANON_KEY =
+        process.env.VITE_API_SUPABASE_ANON_KEY;
+      console.log("Cypress env variables:");
+      console.log(
+        "VITE_API_SUPABASE_URL:",
+        config.env.VITE_API_SUPABASE_URL ? "Configurado" : "No configurado"
+      );
+      console.log(
+        "VITE_API_SUPABASE_ANON_KEY:",
+        config.env.VITE_API_SUPABASE_ANON_KEY ? "Configurado" : "No configurado"
+      );
+
+      return config;
+    },
   },
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,32 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vitest/config";
+import { defineConfig, loadEnv } from "vite";
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: ["./setupTest.ts"],
-    coverage: {
-      provider: "v8",
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: ["./setupTest.ts"],
+      coverage: {
+        provider: "v8",
+      },
     },
-  },
+    define: {
+      "import.meta.env.VITE_API_SUPABASE_URL": JSON.stringify(
+        env.VITE_API_SUPABASE_URL
+      ),
+      "import.meta.env.VITE_API_SUPABASE_ANON_KEY": JSON.stringify(
+        env.VITE_API_SUPABASE_ANON_KEY
+      ),
+    },
+  };
 });
