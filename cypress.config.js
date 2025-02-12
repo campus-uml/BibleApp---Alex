121406e6897d7a4476f0424cdcbe27d7537374ee
soy eddy <soyeddy.email.com>
2025-02-11 19:56:07 -0600

    actualizando configuracion cypress

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   projectId: "2k6vty",
+  env: {
+    SUPABASE_URL: process.env.VITE_SUPABASE_URL,
+    SUPABASE_ANON_KEY: process.env.VITE_SUPABASE_ANON_KEY,
+  },
   e2e: {
     baseUrl: "http://localhost:5173",
   },
