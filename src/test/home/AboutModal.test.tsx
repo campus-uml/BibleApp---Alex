6de4cbfc63f85bc6d1257afa003a69e35d8841fa
soy eddy <soyeddy.email.com>
2025-02-15 11:35:11 -0600

    pruebas en el componete about Modal

--- src/test/home/AboutModal.test.tsx
+++ src/test/home/AboutModal.test.tsx
@@ -1,7 +1,31 @@
-import { describe, expect, test } from "vitest";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { describe, it, expect } from "vitest";
+import "@testing-library/jest-dom";
+import { AboutModal } from "../../components/home/AboutModal";
 
-describe("Pruebas en el componente AboutModal.tsx", () => {
-  test("", async () => {
-    expect(true).toBe(true);
+describe("AboutModal", () => {
+  it("renderiza el botón del modal", () => {
+    render(<AboutModal />);
+    expect(screen.getByRole("button")).toBeInTheDocument();
   });
+
+  it("abre el modal al hacer clic en el botón", async () => {
+    render(<AboutModal />);
+    const button = screen.getByRole("button");
+    await userEvent.click(button);
+    expect(screen.getByText("Acerca del Desarrollador")).toBeInTheDocument();
+  });
+
+  //   it("closes the modal when clicking outside", async () => {
+  //     render(<AboutModal />);
+  //     const button = screen.getByRole("button");
+  //     await userEvent.click(button);
+
+  //     const modalTitle = screen.getByText("Acerca del Desarrollador");
+  //     expect(modalTitle).toBeInTheDocument();
+
+  //     await userEvent.click(document.body);
+  //     expect(modalTitle).not.toBeInTheDocument();
+  //   });
 });
