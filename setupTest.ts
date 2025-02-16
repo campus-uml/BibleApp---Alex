72caad2e5283ba7760fc27c629474872f28dd7e2
soy eddy <soyeddy.email.com>
2025-02-15 19:49:33 -0600

    cargando variables de entorno en setupTest

--- setupTest.ts
+++ setupTest.ts
@@ -1,5 +1,6 @@
-
-import * as matchers from '@testing-library/jest-dom/matchers';
-import { expect } from 'vitest';
+import * as matchers from "@testing-library/jest-dom/matchers";
+import { config } from "dotenv";
+import { expect } from "vitest";
+config();
 
 expect.extend(matchers);
