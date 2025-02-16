fac9773ba2cfd83c1e97d45f3ec628ef9c197058
soy eddy <soyeddy.email.com>
2025-02-16 00:00:22 -0600

    probando renderizado del componente AppSidebar

--- setupTest.ts
+++ setupTest.ts
@@ -1,6 +1,14 @@
 import * as matchers from "@testing-library/jest-dom/matchers";
 import { config } from "dotenv";
-import { expect } from "vitest";
+import { expect, vi } from "vitest";
 config();
 
 expect.extend(matchers);
+
+global.matchMedia = global.matchMedia || function() {
+    return {
+      matches: false, // Default: simulate a non-mobile view (can change to true for mobile)
+      addListener: vi.fn(),
+      removeListener: vi.fn(),
+    };
+  };
---
+++ src/test/components/AppSidebar.test.tsx
@@ -0,0 +1,48 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import { vi } from "vitest";
+import { AppSidebar } from "../../components/AppSidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "@/context/AuthContext";
+import { describe, it, expect } from "vitest";
+import { BibleProvider } from "@/context/BIbleContext";
+
+vi.mock("@/components/Loading", () => ({
+  Loading: () => null, // No mostrar loading en las pruebas
+}));
+
+vi.stubGlobal(
+  "matchMedia",
+  vi.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    addListener: vi.fn(),
+    removeListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  }))
+);
+
+describe("AppSidebar Component", () => {
+  it("should render sidebar after loading", async () => {
+    render(
+      <BrowserRouter>
+        <SidebarProvider>
+          <AuthProvider>
+            <BibleProvider>
+              <AppSidebar />
+            </BibleProvider>
+          </AuthProvider>
+        </SidebarProvider>
+      </BrowserRouter>
+    );
+
+    await waitFor(() => {
+      expect(screen.queryByText("Cargando...")).not.toBeInTheDocument();
+    });
+
+    expect(screen.getByText(/BibleApp/i)).toBeInTheDocument();
+  });
+});
